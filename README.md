e06253ad56f5bf2da1dcd5a73f4f9d1e52faf001
ChidoGang <edgar_quintanilla@mymail.rcbc.edu>
2021-11-04 20:59:58 -0400

    re type node added lib and dist

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Team-Profile-Gen
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)<br/>
+Team Generator is a program that generates a HTML page with the members of your team.
+
+## Table of Contents
+
+1. [Installation](#install)
+2. [Usage](#use)
+3. [Contribute](#cont)
+4. [Testing](#test)
+5. [Questions](#questions)
+6. [License](#license)
+
+## Installation <a id="install"></a>
+
+Use [npm](https://www.npmjs.com/) to install
+
+```bash
+npm install
+```
+
+## Usage <a id="use"></a>
+
+```bash
+node index.js
+```
+
+## Contribute <a id="cont"></a>
+
+If you would like to contribute to the Team Generator please feel free to do so.
+
+## Current Contributors:
+
+
+## Testing <a id="test"></a>
+
+To test the generator, run the program (as shown in [usage](#use)) and enter values in the fields provided.
+
+## Questions <a id="questions"></a>
--- index.js
+++ index.js
@@ -1,173 +1,65 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateHTML = require("./src/generateHTML");
-const render = require("./lib/htmlRenderer")
-const Employee = require("./lib/Employee");
-const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
 const Engineer = require("./lib/Engineer");
-const intern = require("./lib/intern");
-const path = ("path"); 
+const Manager = require("./lib/Manager");
+const generateTeamprofileGenerator = require('./src/page-template')
+const TeamprofileGeneratorArray = [];
 
-const teamArray = [];
-
-const Manager =[
+const askManager = () => {
+    inquirer.prompt([
 {
     type:'input',
     name:'name',
     message:'who is the manager of this team?',
-    validate: nameInput => {
-        if (nameInput) {
-            return true;
-        } else {
-            console.log("Please enter the manager's name"); 
-            return false;
-        }
-    } 
 },
 
 {
     type:'input',
     name:'id',
     message:"Please enter the manager's ID.",
-    validate: nameInput => {
-        if (isNaN(nameInput)) {
-        console.log("Please enter the manager's ID!")
-        return false;
-    } else {
-        return true;
-    }
-    }
-
-},
+ },
 
 {
     type:'input',
     name:'email',
     message:"Please enter manager's email.",
-    validate: email =>{
-        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-        if (valid) {
-            return true;
-        } else {
-            console.log ('Please enter an email!')
-            return false;
-        }
-    }
-
 },
 
 {
 
     type:'input',
     name:'officeNumber',
-    message:"Please enter the manager's office number",
-    validate: nameInput => {
-        if (isNaN(nameInput)) {
-            console.log ('Please enter an office number!')
-            return false;
-        } else {
-            return true; 
-        }
-    }
-}
-]
-
-
-const Engineer =[
-{
-    type:'list',
-    name:'role',
-    message:"Please choose your employee's role",
-    choices: ['Engineer','Intern']
-
+    message:"Please enter the office number",
 },
 
 {
-    type:'input',
-    name:'ID',
-    message:"Please enter the employee's ID! ",
-},
-
-{
-    type:'input',
-    name:'email',
-    message:"please enter the employee's email",
-}, 
-
-{
-    type:'input',
-    name:'Office #',
-    message:'What does the user need to know about using the repo?',
-
-},
-
-{
-    type:'input',
-    name:'GitHub',
-    message:'What does the user need to know about contributing to the repo!',
-
-}
-
-]
-
-const Intern =[
-    {
-        type:'list',
-        name:'role',
-        message:"Please choose your employee's role",
-        choices: ['Engineer','Intern']
-    
-    },
-    
-    {
-        type:'input',
-        name:'ID',
-        message:"Please enter the employee's ID! ",
-    },
-    
-    {
-        type:'input',
-        name:'email',
-        message:"please enter the employee's email",
-    }, 
-    
-    {
-        type:'input',
-        name:'Office #',
-        message:'What does the user need to know about using the repo?',
-    
-    },
-    
-    {
-        type:'input',
-        name:'GitHub',
-        message:'What does the user need to know about contributing to the repo!',
-    
-    }
-    
-    ]
-
-    const question = [
-    {
         type: 'list',
-        name: 'next employee',
-        messages: 'Select the type of team member you would like to add',
-        choices: ['Engineer', 'intern','done']
-    
+        name: 'role',
+        messages: 'Select the type of team member you would like to add or done if your finished',
+        choices: ['Engineer', 'intern','done'] 
+}
+]).then(function(data){
+    let {name,id,email,officenumber,role,choice} = data
+
+    TeamprofileGeneratorArray.push(data);
+
+    if (choice === 'yes') {
+        console.log('|n-------------------------------------------------------/n');
+        askManager();
+    }else {
+        console.log('Generating Team Profile');
+        writeToFile('./dist/index.html', generateTeamprofileGenerator(TeamprofileGeneratorArray));
     }
-
-]
-
-
-
-function writeToFile(fileName, data) {
-    fs.writeFile('newfile.html', render(teamArray), function (err) {
-      if (err) {
-        return console.log('File has been created successfully');
-      }
+})
+}  
+function writeToFile(fileName,data) {
+    fs.writeFile(fileName, data, function(err) {
+        if(err) {
+            console.log(err);
+        }
     });
-  }
-    
+}
 
-init();
+askManager();
 
